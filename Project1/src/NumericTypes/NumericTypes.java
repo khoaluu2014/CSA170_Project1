e2c778960d2b72f0e1e9926a0031cd45544f6cf0
khoaluu2014 <khoaluu2014@gmail.com>
2019-09-13 03:42:29 +0700

    abc

--- Project1/src/NumericTypes/NumericTypes.java
+++ Project1/src/NumericTypes/NumericTypes.java
@@ -6,17 +6,22 @@ package NumericTypes;
 /**
 This program demonstrates how numeric types and operators behave in Java.
 */
-
+import java.util.Scanner;
 public class NumericTypes
 {
 public static void main (String [] args)
 {
 // TASK #2 Create a Scanner object here
 
+Scanner in = new Scanner(System.in);
 
 // Identifier declarations
-final int NUMBER = 2 ;	// Number of scores final int SCORE1 = 100;	// First test score final int SCORE2 = 95;	// Second test score final int BOILING_IN_F = 212; // Boiling temperature int fToC;	// Temperature Celsius
-double average;	// Arithmetic average
+final int NUMBER = 2 ;	// Number of scores 
+final int SCORE1 = 100;	// First test score 
+final int SCORE2 = 95;	// Second test score 
+final int BOILING_IN_F = 212; // Boiling temperature 
+int fToC = 0;	// Temperature Celsius
+double average = 0;	// Arithmetic average
 String output;	// Line of output
  
 
@@ -35,13 +40,23 @@ System.out.println();	// To leave a blank line
 
 // ADD LINES FOR TASK #2 HERE
 // Prompt the user for first name
+System.out.println("Please enter your first name: ");
+String fName = in.nextLine();
 // Read the user's first name
+System.out.println("Your first name is: " + fName);
 // Prompt the user for last name
+System.out.println("Please enter your last name: ");
+String lName = in.nextLine();
 // Read the user's last name
+System.out.println("Your last name is: " + lName);
 // Concatenate the user's first and last names
-// Print out the user's full name System.out.println();	// To leave a blank line
+String fullName = fName + lName;
+// Print out the user's full name
+System.out.println("Your full name is: " + fullName);
+System.out.println();	// To leave a blank line
 // ADD LINES FOR TASK #3 HERE
 // Get the first character from the user's first name
+char firstChar = String.charAt(1);
 // Print out the user's first initial
 // Convert the user's full name to uppercase
 // Print out the user's full name in uppercase
